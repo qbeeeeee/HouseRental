7f720b62791ffa1108b15ff9ca06785c4064ef53
qbeeeeee <foxfireninetails9@gmail.com>
2024-11-10 12:00:41 +0200

    fixed wrong src path for run build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Set the base path to "/" if deploying to root or specify sub-path like "/my-app/"
 });
